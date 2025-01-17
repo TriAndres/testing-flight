0621ef9cf462492f7c4f686d852c378f4bd983a0
Andrei <An.S-1@yandex.ru>
2025-01-17 14:42:44 +0300

    developer 2

--- src/main/java/com/gridnine/testing/service/TaskService.java
+++ src/main/java/com/gridnine/testing/service/TaskService.java
@@ -1,6 +1,13 @@
 package com.gridnine.testing.service;
 
 import com.gridnine.testing.file.FlightFile;
+import com.gridnine.testing.model.Flight;
+import com.gridnine.testing.model.Segment;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskService {
     private final FlightFile flightFile;
@@ -8,17 +15,35 @@ public class TaskService {
     public TaskService(FlightFile flightFile) {
         this.flightFile = flightFile;
     }
-
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void lesson1() {
         System.out.println("1.\tВылет до текущего момента времени");
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        LocalDateTime dateTime = LocalDateTime.now();
+        String time = dateTime.format(fmt);
+        System.out.println("Текущее время: " + time);
+        for (Flight flight : flightFile.getFlights()) {
+            for (Segment segment : flight.getSegments()) {
+                if (sorted(dateTime,segment)) {
+                    System.out.print("\t" + segment);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
+    private boolean sorted(LocalDateTime dateTime,Segment segment) {
+        return dateTime.isBefore(segment.getDepartureDate());
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void lesson2() {
         System.out.println("2.\tИмеются сегменты с датой прилёта раньше даты вылета");
     }
-
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void lesson3() {
         System.out.println("3.\tОбщее время, проведённое на земле превышает два часа (время на земле — это интервал\n" +
                 "\tмежду прилётом одного сегмента и вылетом следующего за ним)");
     }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
