a67ff590cafe867f679bd8236477f2eb0f393e40
Andrei <An.S-1@yandex.ru>
2025-01-18 14:50:06 +0300

    developer 3

--- src/main/java/com/gridnine/testing/service/TaskService.java
+++ src/main/java/com/gridnine/testing/service/TaskService.java
@@ -6,8 +6,7 @@ import com.gridnine.testing.model.Segment;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 public class TaskService {
     private final FlightFile flightFile;
@@ -15,17 +14,21 @@ public class TaskService {
     public TaskService(FlightFile flightFile) {
         this.flightFile = flightFile;
     }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     public void lesson1() {
         System.out.println("1.\tВылет до текущего момента времени");
+
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
         LocalDateTime dateTime = LocalDateTime.now();
         String time = dateTime.format(fmt);
         System.out.println("Текущее время: " + time);
+
         for (Flight flight : flightFile.getFlights()) {
             for (Segment segment : flight.getSegments()) {
-                if (sorted(dateTime,segment)) {
+                if (dateTime.isBefore(segment.getDepartureDate())) {
                     System.out.print("\t" + segment);
+                } else {
+                    System.out.print(" Условие не выполнено. ");
                 }
             }
             System.out.println();
@@ -33,17 +36,34 @@ public class TaskService {
         System.out.println();
     }
 
-    private boolean sorted(LocalDateTime dateTime,Segment segment) {
-        return dateTime.isBefore(segment.getDepartureDate());
-    }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void lesson2() {
         System.out.println("2.\tИмеются сегменты с датой прилёта раньше даты вылета");
+
+        for (Flight flight : flightFile.getFlights()) {
+            for (Segment segment : flight.getSegments()) {
+                if (segment.getDepartureDate().isAfter(segment.getArrivalDate())) {
+                    System.out.print("\t" + segment);
+                } else {
+                    System.out.print(" Условие не выполнено. ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     public void lesson3() {
         System.out.println("3.\tОбщее время, проведённое на земле превышает два часа (время на земле — это интервал\n" +
                 "\tмежду прилётом одного сегмента и вылетом следующего за ним)");
+
+        for (Flight flight : flightFile.getFlights()) {
+            for (Segment segment : flight.getSegments()) {
+                long a1 =  segment.getDepartureDate().getHour();
+                long a2 = segment.getArrivalDate().getHour();
+                System.out.print("Вылет часов:" + a1 + " Прилёт часов:" + a2 + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
